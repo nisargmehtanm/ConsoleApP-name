a0c5d241565634abdd610f0c14aea8c71d1beecb
Nisarg Mehta <nisargmehta.nm@gmail.com>
2020-01-02 13:23:00 +0530

    Add project files.

---
+++ Program1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            string name, country;
+            Console.Write("Please enter your name:");
+            name = Console.ReadLine();
+            Console.Write("Please enter your country:");
+            country = Console.ReadLine();
+            Console.WriteLine("Hello {0} from country {1}!", name, country);
+            Console.ReadLine();
+        }
+    }
+}
